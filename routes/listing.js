af513c89f97b3e71a597c07380e7be4ab7b68f36
AadarshGupta04 <aadarshgupta56789@gmail.com>
2024-06-10 12:19:27 +0530

    changes

--- routes/listing.js
+++ routes/listing.js
@@ -39,6 +39,7 @@ router.get("/:id", wrapAsync(async (req, res) => {
 router.post("/", validateListing, wrapAsync(async (req, res, next) => {
     let newListing = new Listing(req.body.listing);
     await newListing.save();
+    req.flash("success","New Listing Created!");
     res.redirect("/listings");
 }));
 
