b08cd584253b6ca982a2335a038917dcba02e949
AadarshGupta04 <aadarshgupta56789@gmail.com>
2024-07-03 12:22:30 +0530

    changes

--- public/js/map.js
+++ public/js/map.js
@@ -2,12 +2,12 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12',
-    center: [77.2088, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9 // starting zoom
 });
 
-console.log(coordinates);
-
-// const marker = new mapboxgl.Marker()
-// .setLngLat([coordinates])
-// .addTo(map);
+const marker = new mapboxgl.Marker({ color: 'red' })
+.setLngLat(listing.geometry.coordinates)      //Listing.geometry.coordinates
+.setPopup(new mapboxgl.Popup({ offset: 25 })
+.setHTML(`<h4>${listing.location}</h4><p>Exact location will be provided after booking.</p>`))
+.addTo(map);
